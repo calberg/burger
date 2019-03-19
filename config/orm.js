59f2032879adf4908541a1c4d5bfbbc0d1820c1a
Calberg89 <chris_alberg@yahoo.com>
2019-03-19 13:32:44 -0700

    Updated file

---
+++ config/orm.js
@@ -0,0 +1,58 @@
+var connection = require("../config/connection.js")
+var map = require("../config/orm.js")
+
+function selectAll() {
+
+}
+function insertOne() {
+
+}
+function updateOne() {
+
+}
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
